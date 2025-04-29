0f25c6c113ef09a91ecd590dc034d75c3e047477
Sk23 <166255245+Skepper23@users.noreply.github.com>
2025-04-29 17:00:26 -0400

    update

---
+++ leb.js
@@ -0,0 +1,10 @@
+//hi
+
+
+window.onload = function() {
+    const chance = 0.1;
+
+    if (Math.random() < chance) {
+      document.body.innerHTML = `<img src="img/leb.jpg" style="width: 100vw; height: 100vh; object-fit: cover;">`;
+    }
+  };
